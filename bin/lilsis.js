48e4ca0315f9b243d2faa8e3195d0b470425696d
Christophe Eymard <christophe.eymard@ravelsoft.com>
2012-03-22 14:59:28 +0100

    Some more work

--- bin/lilsis.js
+++ bin/lilsis.js
@@ -1,6 +1,25 @@
-// Call to streamline to streamline all the modules.
-if (!require('streamline/module')(module)) return;
+#!/usr/bin/env node
+
+var opt = require("optimist")
+    .options('c', {
+        alias: 'client-only',
+        default: false
+    })
+    .options('w', {
+        alias: 'web-only',
+        default: false
+    })
+var argv = opt.argv
 
 var lilsis = require("lilsis")
 
-lilsis.startMonitoring(_)
+// Only start the client
+if (!argv.w) {
+    console.log("Starting monitoring.")
+    lilsis.startMonitoring(function(err, stat) {})
+}
+
+// Only start the webserver.
+if (!argv.c) {
+    lilsis.startWebServer()
+}
